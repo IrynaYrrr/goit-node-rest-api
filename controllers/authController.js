709810477dec61bec62e152ec3f843d688b2a135
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-03-22 22:19:59 +0100

    add email check

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,13 @@ import { HttpError } from '../helpers/HttpError.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
 
 const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await authServices.findUser({ email });
+  if (user) {
+    throw HttpError(409, 'Email in use');
+  }
   const newUser = await authServices.signup(req.body);
+
 
   res.status(201).json({
     username: newUser.username,
--- services/authServices.js
+++ services/authServices.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
 
+export const findUser = filter => User.findOne(filter);
 
 export const signup = data => User.create(data);
